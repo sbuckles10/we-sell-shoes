b1a53be95b3bc5f00752288b6bcbecb280180b0b
sbuckles10 <sbuckles88@gmail.com>
2021-10-19 15:09:00 -0700

    update

--- index.js
+++ index.js
@@ -97,24 +97,47 @@ for (let i = 0; i < acc.length; i++) {
   //trigger an event after click
   acc[i].addEventListener("click", function() {
 
-    //add in the active class
     this.classList.toggle("active");
-    //select sibling element
+
     let panel = this.nextElementSibling;
+
+
     //if sibling is open, close it, if closed, open it
     if (panel.style.maxHeight){
   //panel is open open
-  panel.style.maxHeight = null; //add this line
-
-} else {
-  //panel is closed
-  panel.style.maxHeight = panel.scrollHeight + "px"; //add this line
-}
-      //panel is open
-
+      panel.style.maxHeight = null;
     } else {
-      //panel is closed
-
+  //panel is closed
+      panel.style.maxHeight = panel.scrollHeight + "px"; //add this line
     }
   });
 }
+/////////popup//////////////
+
+//declare default vars
+const popUp = document.getElementById('pop-up');
+const popUpLayer = document.getElementById('pop-up-layer');
+const closeModal = document.getElementById('close-pop-up');
+
+//nav-bar button
+const subBtn = document.getElementById('sub-btn');
+//navbar button click makes the popup appear
+subBtn.addEventListener("click", () => {
+  popUpLayer.style.display = 'flex';
+})
+
+//close model click makes the popup disappear
+closeModal.addEventListener("click", function() {
+  popUpLayer.style.display = 'none';
+})
+let popUpSeconds = 40;
+
+//trigger popup after seconds variable
+let popUpFun = window.setInterval(function() {
+  popUpLayer.style.display = 'flex';
+
+  window.clearInterval(popUpFun);
+
+}, popUpSeconds * 1000);
+
+///////////////////
